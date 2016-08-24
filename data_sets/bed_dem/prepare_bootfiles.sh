72fc40bd428b27b1c72b5d5fa13529f056a22476
Andy Aschwanden <aaschwanden@alaska.edu>
2016-08-23 17:37:49 -0800

    playing with extended domain

--- data_sets/bed_dem/prepare_bootfiles.sh
+++ data_sets/bed_dem/prepare_bootfiles.sh
@@ -16,22 +16,42 @@ for GRID  in 50 100 200 250 500 1000 2000 5000; do
     gdalwarp -overwrite -of netCDF -co "FORMAT=NC4" -r average -s_srs EPSG:26710 -t_srs EPSG:26710 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID $infile $outfile
     ncrename -v Band1,topg $outfile
     # ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a proj4,global,o,c,"+init=EPSG:26710" $outfile
-    ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" $outfile
+    ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a _FillValue,topg,d,, $outfile
+    ncap2 -O -s "where(topg<0) {topg=-0.;};"  $outfile  $outfile
 done
 
+# # Domain size
+# xmin=400000
+# ymin=5231000
+# xmax=520000
+# ymax=5331000
+
+# version=1
+
+
+# for GRID  in 50 100 200 250 500 1000 2000 5000; do
+#     outfile=pism_Olympics_mtns_${GRID}m_v${version}.nc
+#     gdalwarp -overwrite -of netCDF -co "FORMAT=NC4" -r average -s_srs EPSG:26710 -t_srs EPSG:26710 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID $infile $outfile
+#     ncrename -v Band1,topg $outfile
+#     # ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a proj4,global,o,c,"+init=EPSG:26710" $outfile
+#     ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" $outfile
+# done
+
+
 # Domain size
-xmin=400000
-ymin=5231000
-xmax=520000
-ymax=5331000
+xmin=250000
+ymin=5001000
+xmax=600000
+ymax=5581000
 
 version=1
 
 
-for GRID  in 50 100 200 250 500 1000 2000 5000; do
-    outfile=pism_Olympics_mtns_${GRID}m_v${version}.nc
+for GRID  in 100 200 250 500 1000 2000 5000; do
+    outfile=pism_Olympics_ext_${GRID}m_v${version}.nc
     gdalwarp -overwrite -of netCDF -co "FORMAT=NC4" -r average -s_srs EPSG:26710 -t_srs EPSG:26710 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID $infile $outfile
     ncrename -v Band1,topg $outfile
+    ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a _FillValue,topg,d,, $outfile
     # ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a proj4,global,o,c,"+init=EPSG:26710" $outfile
-    ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" $outfile
+    ncap2 -O -s "where(topg<0) {topg=-0.;};"  $outfile  $outfile
 done
