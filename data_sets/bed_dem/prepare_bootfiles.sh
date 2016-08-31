2181719e451fd4e292ae1f898574f37deb21ee37
Alison <amanders@illinois.edu>
2016-08-31 15:19:06 -0500

    First attempts at this

--- data_sets/bed_dem/prepare_bootfiles.sh
+++ data_sets/bed_dem/prepare_bootfiles.sh
@@ -13,13 +13,15 @@ infile=Olympics_30m.tif
 
 for GRID  in 50 100 200 250 500 1000 2000 5000; do
     outfile=pism_Olympics_${GRID}m_v${version}.nc
-    gdalwarp -overwrite -of netCDF -co "FORMAT=NC4" -r average -s_srs EPSG:26710 -t_srs EPSG:26710 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID $infile $outfile
+#    gdalwarp -overwrite -of netCDF -co "FORMAT=NC4" -r average -s_srs EPSG:26710 -t_srs EPSG:26710 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID $infile $outfile
+    gdalwarp -overwrite -of GTiff -r average -s_srs EPSG:26710 -t_srs EPSG:26710 -te $xmin $ymin $xmax $ymax -tr $GRID $GRID $infile tmp_${GRID}.tif
+    gdal_translate -of netCDF -co "FORMAT=NC4" tmp_${GRID}.tif $outfile
     ncrename -v Band1,topg $outfile
     # ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a proj4,global,o,c,"+init=EPSG:26710" $outfile
     ncatted -a standard_name,topg,o,c,"bedrock_altitude" -a units,topg,o,c,"m" -a _FillValue,topg,d,, $outfile
     ncap2 -O -s "where(topg<0) {topg=-0.;};"  $outfile  $outfile
 done
-
+exit
 # # Domain size
 # xmin=400000
 # ymin=5231000
