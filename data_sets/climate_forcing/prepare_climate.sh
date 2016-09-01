ed094a7f18e82de27d7f9cd310961e7346fe3764
Andy Aschwanden <aaschwanden@alaska.edu>
2016-09-01 13:28:30 -0800

    backup parallel version

--- data_sets/climate_forcing/prepare_climate.sh
+++ data_sets/climate_forcing/prepare_climate.sh
@@ -3,7 +3,7 @@ set -x -e
 
 for GRID  in 100 200 250 500 1000; do
     outfile=ltop_climate_olympics_${GRID}m_mm_hr-1.nc
-    python ~/base/flowline-glacier-model/linear_orog_precip.py -i ../bed_dem/pism_Olympics_${GRID}m_v1.nc -o $outfile --tau_c 933 --tau_f 933 --vapor_scale_height 3200 --wind_direction 249.1 --wind_magnitude 22.1 --background_precip 10 --no_trunc --precip_scale_factor 0.04
+    python ../../scripts/linear_orog_precip.py -i ../bed_dem/pism_Olympics_${GRID}m_v1.nc -o $outfile --tau_c 933 --tau_f 933 --vapor_scale_height 3200 --wind_direction 249.1 --wind_magnitude 22.1 --background_precip 10 --no_trunc --precip_scale_factor 0.04
     ncrename -v Band1,precipitation $outfile
     ncap2 -O -s "air_temp[\$y,\$x]=10.5f; usurf[\$y,\$x]=0.f;" $outfile $outfile
     ncatted -a units,precipitation,o,c,"mm hr-1" -a units,air_temp,o,c,"celsius" -a units,usurf,o,c,"m" -a standard_name,usurf,o,c,"surface_altitude" $outfile
