b36d22e8d09ec793c3df9642a9fa846e0732a10d
Andy Aschwanden <aaschwanden@alaska.edu>
2017-02-22 17:38:11 -0900

    work on dT modifier

--- run/preprocess.sh
+++ run/preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for T in -5 -4 -3; do
-    ncap2 -O -s "delta_T=${T};" paleo_modifier.nc paleo_modifier_${T}K.nc
+    ncap2 -O -s "delta_T(0)=${T};" paleo_modifier.nc paleo_modifier_${T}K.nc
 done
 
