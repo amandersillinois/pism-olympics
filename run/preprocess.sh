7b1dbca31f0c6f3371da5f45133c9fbcd1027de3
Andy Aschwanden <aaschwanden@alaska.edu>
2017-04-25 14:52:03 -0800

    added uplate rates shape file added glacial cycle script

--- run/preprocess.sh
+++ run/preprocess.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ncap2 -O -s "delta_T=0.4*delta_T;" pism_dT.nc pism_scaled_dT.nc
+
 for T in -7 -6 -5 -4; do
     ncap2 -O -s "delta_T(0)=${T};" paleo_modifier.nc paleo_modifier_${T}K.nc
 done
