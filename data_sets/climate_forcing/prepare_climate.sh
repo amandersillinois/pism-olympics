027fa5d00e7bd5526f1299c5e6b63471f124578a
Andy Aschwanden <aaschwanden@alaska.edu>
2018-08-10 15:20:09 +0200

    updated prepare script

--- data_sets/climate_forcing/prepare_climate.sh
+++ data_sets/climate_forcing/prepare_climate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x -e 
 
-#for GRID  in 50 100 200 250 500 1000 2000; do
-for GRID  in 1000; do
+for GRID  in 50 100 200 250 500 1000 2000; do
+#for GRID  in 1000; do
     for dir in 220; do
         outfile=ltop_climate_olympics_${GRID}m_dir_${dir}_kg_m-2_yr-1.nc
         python ../../scripts/linear_orog_precip.py -i ../bed_dem/pism_Olympics_${GRID}m_v1.nc --background_precip_pre 1 --background_precip_post 0.057 --precip_scale_factor 0.125 --tau_c 1750 --tau_f 1750 --wind_direction $dir --wind_magnitude 15 --moist_stability 0.007 --vapor_scale_height 2600 -o $outfile --o_units 'kg m-2 yr-1' --water_mass_rate
